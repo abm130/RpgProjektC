916de8b7ce085365eaf25ad1b5b2b308e5103ddc
Unknown <waldekjulez@gmail.com>
2017-04-24 14:44:38 +0200

    Loader Skript erstellt

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Dafür da um zu checken ob ein GameManager instantiiert wurde
+public class Loader : MonoBehaviour {
+
+	public GameObject gameManager;
+	
+	// Use this for initialization
+	void Awake () {
+		if(GameManager.instance == null){
+			Instantiate(gameManager);
+		}
+			
+	}
+	
+}
