0ac82d4bc37834b7338e3cdbc94fc47d6ed8a670
Unknown <waldekjulez@gmail.com>
2017-04-24 13:25:41 +0200

    GameManager Skript funktionsfähig gemacht

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-
+	
+	/* GameManager wird zum Singleton gemacht
+	Da er nur 1 mal benötigt wird */
+	public static GameManager instance = null;
+	
+	public BoardManager boardScript;
+	
+	private int level = 3;
+	
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		}
+		else if (instance != this){
+			Destroy(gameObject);
+		}
+		//Damit bei Szenenwechsel der GameManager nicht terminiert wird
+		DontDestroyOnLoad(gameObject);
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+	
+	void InitGame() {
+		boardScript.SetupScene(level);
 	}
 	
 	// Update is called once per frame
